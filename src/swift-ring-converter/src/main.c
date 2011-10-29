0b5b7fb5bc20899b88e33650a103813ee529eda3
Mukhitov <dmitriy.mukhitov@gmail.com>
2011-10-29 17:14:46 +0400

    regexp parsing file name

--- src/swift-ring-converter/src/main.c
+++ src/swift-ring-converter/src/main.c
@@ -9,7 +9,28 @@
 #include <string.h>
 #include <stdio.h>
 #include <getopt.h>
+#include <pcreposix.h>
 #include "PythonDeserialize.h"
+
+void parseFileName(const char* inpStr, char* fileName) {
+	char *pattern = "(\\w+)\\.ring\\.gz";
+	regmatch_t p[20];
+	regex_t f;
+	if (regcomp(&f, pattern, 0)) /* если возвращаемое значение не 0 - ошибка */
+	{
+		printf("Error in pattern! %s\n", pattern);
+		exit(1);
+	}
+	if (regexec(&f, inpStr, 20, p, 0)) {
+		printf("Invalid file name!\n");
+		exit(1);
+	}
+
+	int i,j;
+	for (i=0,j = p[1].rm_so; j < p[1].rm_eo; j++, i++)
+		fileName[i]=inpStr[j];
+	fileName[i+1]='\0';
+}
 
 int main(int argc, char *argv[]) {
 	char fileName[1024];
@@ -32,17 +53,17 @@ int main(int argc, char *argv[]) {
 		};
 	};
 
+	char name[256];
+	parseFileName(fileName,name);
+
+	char newName[1024];
+
+	strcpy(newName,name);
+	strcat(newName,".tpl");
+
 	Ring rng = deserializeRing(fileName);
-	save("ring.tpl", &rng);
-	Ring rng2 = load("ring.tpl");
-	save("ring2.tpl", &rng2);
-
-
-	if(!equal(&rng,&rng2))
-		printf("ring is not equal\n");
-
+	save(newName, &rng);
 	destroy(&rng);
-	destroy(&rng2);
 	return 0;
 }
 
